9ad39a250b946017211deea0a53d454efe0bc847
zwright95 <33302292+zwright95@users.noreply.github.com>
2018-04-23 22:42:42 -0400

    Many changes
    Added buttons for saving and draw. Added prompt for what type of game. Added checkmate, check, stalemate function usage. Added function to end the game.

--- Game.java
+++ Game.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.awt.event.*;
+
 
 public class Game{
 	public static void main(String [] args){
 		JFrame frame = new JFrame("Xiangqi: Chinese Chess");
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		StartGame game = new StartGame();
+		int answer = JOptionPane.showConfirmDialog(frame, "Would you like to load the last saved game?");
+		StartGame game;
+		game = new StartGame(answer);
 		frame.add(game);
-		frame.setSize(600, 650);
+		frame.setSize(700, 750);
 		frame.setVisible(true);
 	}
 }
@@ -24,64 +26,146 @@ class StartGame extends JPanel{
 	Pair pair;			//used for moving a piece to a point
 	int currentPlayer;
 	String printStr;	//string that will be printed on screen
-	public StartGame(){
+	boolean endGame;
+	JButton button1;
+	JButton button2;
+	Pair oldCoords; 	//used if a player puts themselves in Check
+	public StartGame(int newGame){
+		if(newGame == JOptionPane.NO_OPTION)//value is 1
+		{
+			System.out.println("New");
+		}	
+		else if(newGame == JOptionPane.YES_OPTION)//load game data value is 0
+		{//pass in something to the Pieces() object to load the previous game
+			//set currentPlayer to correct player
+		}
+		else
+		{
+			System.exit(0);
+		}
 		board = new DrawBoard();
 		pieces = new Pieces();
 		pointsPressed = 0;
 		pair = null;
+		oldCoords = new Pair();
 		currentPlayer = 1;
 		printStr = "";
+		endGame = false;
+		button1 = new JButton("Save");
+		button2 = new JButton("Forfeit");
+		add(button1);
+		add(button2);
+		button1.addActionListener(new ActionListener()
+		{
+			//save game
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{//only need to store the arraylist of pieces and the current player
+
+			}
+
+		});
+		button2.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				getOpposite();
+				printStr = "Player " + currentPlayer + ", you win!";
+				finishGame();
+			}
+		});
 		addMouseListener(new MouseAdapter() { //mouse clicking
       		public void mousePressed(MouseEvent event) {
-        	System.out.println(event.getPoint());	//get point pressed
-        	pointsPressed++;
-        	if(pointsPressed == 1){	//if selecting a piece
-        		piece = pieces.validPiece(event.getPoint());
-        		if(piece.getX() != 100 && piece.getY() != 100 )//will be 100, 100 if not a valid piece
-        		{
-        			if(piece.getPlayer() != currentPlayer)//piece was selected but wrong player
-        			{
-		    			printStr = "Wrong player's piece!";
-		    			pointsPressed--;
-        			}
-        			else//valid piece was selected 
-        				printStr = "Selected piece: " + piece.getX() + ", " + piece.getY();
+      		if(endGame != true){
+	        	System.out.println(event.getPoint());	//get point pressed
+	        	pointsPressed++;
+	        	if(pointsPressed == 1){	//if selecting a piece
+	        		piece = pieces.validPiece(event.getPoint());
+	        		if(piece.getX() != 100 && piece.getY() != 100 )//will be 100, 100 if not a valid piece
+	        		{
+	        			if(piece.getPlayer() != currentPlayer)//piece was selected but wrong player
+	        			{
+			    			printStr = "Wrong player's piece!";
+			    			pointsPressed--;
+	        			}
+	        			else{
+	        				//valid piece was selected
+	        				printStr = "Selected piece: " + piece.getX() + ", " + piece.getY();
+	        				oldCoords.x = piece.getX();
+	        				oldCoords.y = piece.getY();
+	        			}
+	        		}
+	        		else{//print error message and decrease points pressed to reselect piece
+	        			printStr = "Not a valid piece!";
+	        			pointsPressed--;
+	        		}
+	        		repaint();//repaint to show the error message
+	        	}
+	        	else if(pointsPressed == 2){//if selecting a move position
+	        		pair = pieces.getSpot(event.getPoint());//get the spot on the board that was selected
+	        		pointsPressed = 0;
+	        		printStr = "Invalid movement, reselect piece!"; //will only remain like this if player moved wrong
+	        		if(pair.x != 100 && pair.y != 100)//check if a valid position was selected
+	        		{
+	        			System.out.println("Valid");
+	        			ArrayList<Pair> moves = piece.availableMove(pieces);	//find the players available moves
+	        			for (int i = 0; i < moves.size(); ++i)
+					    {
+					    	Pair move = moves.get(i);
+					        System.out.print("Move: ");
+					        System.out.println(move);
+					        if(move.x == pair.x && move.y == pair.y)//move position was valid
+					        {//going to move the piece here
+					        	String ret = pieces.movePiece(piece, pair, oldCoords);
+					        	if(!ret.equals("Cannot put self in check!")){
+						        	printStr = ret;	//get the return value to let the player know what type of move happened
+						        	getOpposite(); //change player and change turn
+
+						        	//start of new player move
+						        	if(pieces.check(currentPlayer) == true)//check if current player is in check
+						        	{
+						        		printStr = "Player " + currentPlayer + " you are in check!";
+						        	}
+
+						        	if(pieces.checkMate(currentPlayer) == true){ //check if current player is in checkmate
+						        			getOpposite();
+						        			printStr = "Player " + currentPlayer + ", you win!";
+						        			endGame = true;
+						        			repaint();
+						        			finishGame(); //end game 
+						        	}
+
+						        	if(pieces.staleMate(currentPlayer) == true)
+						        	{
+						        		getOpposite();
+						        		printStr = "Player " + currentPlayer + ", you win!";
+						        		endGame = true;
+						        		repaint();
+						        		finishGame(); //end game 
+						        	}
+					       		}
+					       		printStr = ret;
+					        	break;
+					        }
+					    }
+	        			repaint();	//repaint board to show new pieces and new string message
+	        		}
+	        		else
+	        			System.out.println("Invalid");
         		}
-        		else{//print error message and decrease points pressed to reselect piece
-        			printStr = "Not a valid piece!";
-        			pointsPressed--;
-        		}
-        		repaint();//repaint to show the error message
-        	}
-        	else if(pointsPressed == 2){//if selecting a move position
-        		pair = pieces.getSpot(event.getPoint());//get the spot on the board that was selected
-        		pointsPressed = 0;
-        		printStr = "Invalid movement, reselect piece!"; //will only remain like this if player moved wrong
-        		if(pair.x != 100 && pair.y != 100)//check if a valid position was selected
-        		{
-        			System.out.println("Valid");
-        			ArrayList<Pair> moves = piece.availableMove(pieces);	//find the players available moves
-        			for (int i = 0; i < moves.size(); ++i)
-				    {
-				    	Pair move = moves.get(i);
-				        System.out.print("Move: ");
-				        System.out.println(move);
-				        if(move.x == pair.x && move.y == pair.y)//move position was valid
-				        {//going to move the piece here
-				        	String ret = pieces.movePiece(piece, pair);
-				        	printStr = ret;	//get the return value to let the player know what type of move happened
-				        	getOpposite(); //change player
-				        	break;
-				        }
-				    }
-        			repaint();	//repaint board to show new pieces and new string message
-        		}
-        		else
-        			System.out.println("Invalid");
-        	}
+      		}
       	}
     	});
 		repaint();
+	}
+
+	public void finishGame()
+	{
+		 int n = JOptionPane.showOptionDialog(null,printStr, "Game Over!", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+		 if(n == 0 || n == 1 || n == -1){
+		 	System.exit(0);
+		 }  
 	}
 
 	public void getOpposite()
@@ -101,8 +185,8 @@ class StartGame extends JPanel{
 
 		g2d.setColor(Color.BLACK);
 		//draw player string and printing string
-		g2d.drawString("Player " + currentPlayer + "'s turn!", 250, 30);
-		g2d.drawString(printStr, 250, 575);
+		g2d.drawString("Player " + currentPlayer + "'s turn!", 300, 60);
+		g2d.drawString(printStr, 300, 700);
 
 	}
 }
